e98f345a73d1d8c072ed665723e452f7b9069fde
Jonathan Rux <jonathan.e.rux@ruxunderscore.com>
2024-03-17 04:45:22 -0700

    test

--- scpLogs.py
+++ scpLogs.py
@@ -9,18 +9,26 @@
 #                                                       |                                                     #
 ###############################################################################################################
 
+import getpass, paramiko
+from stat import S_ISDIR
 
-from getpass import getpass
-import paramiko
+paramiko.util.log_to_file("./tmp/paramiko.log")
 
+# Get Target Systems Information from user.
 def getClientInfo():
+    host = input("Please enter the hostname or IP of the target system: ")
+    user = input("Please enter the username for the target system: ")
+    passwd = getpass.getpass(prompt=f"Please enter the password for {user}@{host}: ")
+    return host, user, passwd
     
-
+# Main Function
 def main():
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    host, user, pass = getClientInfo()
-    client.connect(host, username=$user, password=$pass)
+    host, user, passwd = getClientInfo()
+    print(f"Using: {user}@{host}")
+    client.connect(host, username=user, password=passwd)
 
+# Decided to play with the if __name__ == "__main__" system.
 if __name__ == "__main__":
     main()
